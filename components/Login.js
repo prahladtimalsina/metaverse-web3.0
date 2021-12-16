1bb03683d085bf165dd3de9ae4db45ae501166f5
prahladtimalsina <prahladtimalsina7@gmail.com>
2021-12-16 22:57:41 +0545

    code refractor

--- components/Login.js
+++ components/Login.js
@@ -1,33 +1,40 @@
 import Image from "next/image";
 import { useMoralis } from "react-moralis";
+import Head from 'next/head'
 
 function Login() {
   const { authenticate } = useMoralis();
 
   return (
-    <div className="bg-black relative text-white">
-      <div className="flex flex-col absolute z-50 h-4/5 w-full items-center justify-center space-y-4">
-        <Image
-          className="object-cover rounded-full"
-          src="https://links.papareact.com/3pi"
-          height={200}
-          width={200}
-        />
-        <button
-          onClick={authenticate}
-          className="bg-yellow-500 rounded-lg p-5 font-bold animate-pulse"
-        >
-          Login to the METAVERSE
-        </button>
-        {/* logo and button */}
-      </div>
-      <div className="w-full h-screen">
-        <Image
-          src="https://links.papareact.com/55n"
-          layout="fill"
-          objectFit="cover"
-        />
-        {/* background image */}
+    <div className="h-screen">
+      <Head>
+        <title>Metaverse Challenge</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="bg-black relative text-white">
+        <div className="flex flex-col absolute z-50 h-4/5 w-full items-center justify-center space-y-4">
+          <Image
+            className="object-cover rounded-full"
+            src="https://links.papareact.com/3pi"
+            height={200}
+            width={200}
+          />
+          <button
+            onClick={authenticate}
+            className="bg-yellow-500 rounded-lg p-5 font-bold animate-pulse"
+          >
+            Login to the METAVERSE
+          </button>
+          {/* logo and button */}
+        </div>
+        <div className="w-full h-screen">
+          <Image
+            src="https://links.papareact.com/55n"
+            layout="fill"
+            objectFit="cover"
+          />
+          {/* background image */}
+        </div>
       </div>
     </div>
   );
--- pages/index.js
+++ pages/index.js
@@ -6,38 +6,29 @@ export default function Home() {
   const { isAuthenticated, logout } = useMoralis();
   if (!isAuthenticated) return <Login />;
   return (
-    // <div className="h-screen">
-    //   <Head>
-    //     <title>Metaverse Challenge</title>
-    //     <link rel="icon" href="/favicon.ico" />
-    //   </Head>
-
-    //   <h1>Welcome </h1>
-    //   <button
-    //       onClick={logout}
-    //       className="bg-yellow-500 rounded-lg p-5 font-bold animate-pulse"
-    //     >
-    //       Logout from METAVERSE
-    //     </button>
-    // </div>
-    <div className="bg-black relative text-white">
-      <div className="flex flex-col absolute z-50 h-4/5 w-full items-center justify-center space-y-4">
-        
+    <div className="h-screen">
+      <Head>
+        <title>Metaverse Challenge</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="bg-black relative text-white">
+        <div className="flex flex-col absolute z-50 h-4/5 w-full items-center justify-center space-y-4">
           <button
-          onClick={logout}
-          className="bg-yellow-500 rounded-lg p-5 font-bold animate-bounce"
-        >
-          Logout from METAVERSE
-        </button>
-        {/* logo and button */}
-      </div>
-      <div className="w-full h-screen">
-        <Image
-          src="https://links.papareact.com/55n"
-          layout="fill"
-          objectFit="cover"
-        />
-        {/* background image */}
+            onClick={logout}
+            className="bg-yellow-500 rounded-lg p-5 font-bold animate-bounce"
+          >
+            Logout from METAVERSE
+          </button>
+          {/* logo and button */}
+        </div>
+        <div className="w-full h-screen">
+          <Image
+            src="https://links.papareact.com/55n"
+            layout="fill"
+            objectFit="cover"
+          />
+          {/* background image */}
+        </div>
       </div>
     </div>
   );
