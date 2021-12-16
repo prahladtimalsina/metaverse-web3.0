579a60442fffe82de5f70e7c5ff26fba79f84c08
prahladtimalsina <prahladtimalsina7@gmail.com>
2021-12-16 17:19:51 +0545

    login page layout done

--- components/Login.js
+++ components/Login.js
@@ -1,9 +1,27 @@
+import Image from "next/image";
+
 function Login() {
-    return (
-        <div className="bg-black relative">
-            <h1>i am the login screen</h1>
-        </div>
-    )
+  return (
+    <div className="bg-black relative text-white">
+      <h1>i am the login screen</h1>
+      <div className="flex flex-col absolute z-50 h-4/5 w-full items-center justify-center space-y-4">
+        <Image 
+        
+        className="object-cover rounded-full"
+        src="https://links.papareact.com/3pi" height={200} width={200} />
+        <button className="bg-yellow-500 rounded-lg p-5 font-bold animate-pulse">Login to the METAVERSE</button>
+        {/* logo and button */}
+      </div>
+      <div className="w-full h-screen">
+        <Image
+          src="https://links.papareact.com/55n"
+          layout="fill"
+          objectFit="cover"
+        />
+        {/* background image */}
+      </div>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
--- pages/index.js
+++ pages/index.js
@@ -2,15 +2,16 @@ import Head from 'next/head'
 import Login from "../components/Login";
 
 export default function Home() {
+  const  isAuthenticated = false;
+  if(!isAuthenticated) return <Login />
   return (
-    <div className="">
+    <div className="h-screen">
       <Head>
         <title>Metaverse Challenge</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <h1>welocme to metaverse challenge</h1>
-      <Login />
+     <h1>Welcome to the app</h1>
     </div>
   );
 }
