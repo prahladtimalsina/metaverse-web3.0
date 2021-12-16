2101933a327d8e88720ebf04e60efabf1a0c2a16
prahladtimalsina <prahladtimalsina7@gmail.com>
2021-12-17 01:10:29 +0545

    add pic

--- components/Login.js
+++ components/Login.js
@@ -8,20 +8,21 @@ function Login() {
   return (
     <div className="h-screen">
       <Head>
-        <title>Metaverse Challenge</title>
+        <title>Metaverse | web3.0</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className="bg-black relative text-white">
         <div className="flex flex-col absolute z-50 h-4/5 w-full items-center justify-center space-y-4">
           <Image
             className="object-cover rounded-full"
-            src="https://links.papareact.com/3pi"
+            src="/images/prahlad-timalsina.jpg"
             height={200}
             width={200}
+            
           />
           <button
             onClick={authenticate}
-            className="bg-yellow-500 rounded-lg p-5 font-bold animate-pulse"
+            className="bg-yellow-500 rounded-lg p-5 font-bold animate-bounce"
           >
             Login to the METAVERSE
           </button>
