60afbe4609eb016bd44b1f8ec617aa099766d155
prahladtimalsina <prahladtimalsina7@gmail.com>
2021-12-16 17:57:10 +0545

    moralis authenticaed part done

--- components/Login.js
+++ components/Login.js
@@ -1,15 +1,25 @@
 import Image from "next/image";
+import { useMoralis } from "react-moralis";
 
 function Login() {
+  const { authenticate } = useMoralis();
+
   return (
     <div className="bg-black relative text-white">
       <h1>i am the login screen</h1>
       <div className="flex flex-col absolute z-50 h-4/5 w-full items-center justify-center space-y-4">
-        <Image 
-        
-        className="object-cover rounded-full"
-        src="https://links.papareact.com/3pi" height={200} width={200} />
-        <button className="bg-yellow-500 rounded-lg p-5 font-bold animate-pulse">Login to the METAVERSE</button>
+        <Image
+          className="object-cover rounded-full"
+          src="https://links.papareact.com/3pi"
+          height={200}
+          width={200}
+        />
+        <button
+          onClick={authenticate}
+          className="bg-yellow-500 rounded-lg p-5 font-bold animate-pulse"
+        >
+          Login to the METAVERSE
+        </button>
         {/* logo and button */}
       </div>
       <div className="w-full h-screen">
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
 import Head from 'next/head'
 import Login from "../components/Login";
-
+import { useMoralis } from "react-moralis";
 export default function Home() {
-  const  isAuthenticated = false;
-  if(!isAuthenticated) return <Login />
+  const { isAuthenticated, logout } = useMoralis();
+  if (!isAuthenticated) return <Login />;
   return (
     <div className="h-screen">
       <Head>
@@ -11,7 +11,8 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-     <h1>Welcome to the app</h1>
+      <h1>Welcome to the app</h1>
+      <button onClick={logout}>Logout</button>
     </div>
   );
 }
