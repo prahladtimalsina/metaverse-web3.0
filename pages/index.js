17bcf01f164c36eff37f4c07052d8ff509a8801f
prahladtimalsina <prahladtimalsina7@gmail.com>
2021-12-19 01:13:59 +0545

    moralis message done

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Login from "../components/Login";
 import { useMoralis } from "react-moralis";
 import Image from 'next/image';
 import Header from "../components/Header";
+import Messages from "../components/Messages";
 export default function Home() {
   const { isAuthenticated } = useMoralis();
   if (!isAuthenticated) return <Login />;
@@ -17,9 +18,8 @@ export default function Home() {
       </Head>
 
       <div className="max-w-screen-2xl mx-auto">
-        {/* Header */}
         <Header />
-        {/* Messages */}
+        <Messages />
       </div>
     </div>
   );
