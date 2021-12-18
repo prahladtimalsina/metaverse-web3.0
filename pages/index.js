f2d23f7a6528ec4e20182c2915e6a1e1fc0d1c23
prahladtimalsina <prahladtimalsina7@gmail.com>
2021-12-18 22:45:52 +0545

    dicebar api used

--- pages/index.js
+++ pages/index.js
@@ -2,34 +2,27 @@ import Head from 'next/head'
 import Login from "../components/Login";
 import { useMoralis } from "react-moralis";
 import Image from 'next/image';
+import Header from "../components/Header";
 export default function Home() {
   const { isAuthenticated, logout } = useMoralis();
   if (!isAuthenticated) return <Login />;
   return (
-    <div className="h-screen">
+    <div
+      className="h-screen overflow-y-scroll bg-gradient-to-b 
+    from-black to-fuchsia-900 overflow-hidden"
+    >
       <Head>
         <title>Metaverse Challenge</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className="bg-black relative text-white">
-        <div className="flex flex-col absolute z-50 h-4/5 w-full items-center justify-center space-y-4">
-          <button
-            onClick={logout}
-            className="bg-yellow-500 rounded-lg p-5 font-bold animate-bounce"
-          >
-            Logout from METAVERSE
-          </button>
-          {/* logo and button */}
-        </div>
-        <div className="w-full h-screen">
-          <Image
-            src="https://links.papareact.com/55n"
-            layout="fill"
-            objectFit="cover"
-          />
-          {/* background image */}
-        </div>
+
+      <div className="max-w-screen-2xl mx-auto">
+        {/* Header */}
+        <Header />
+        {/* Messages */}
       </div>
+
+      <button onClick={logout}>Logout</button>
     </div>
   );
 }
